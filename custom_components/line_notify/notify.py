a771c534013786317406fcb1807d16bbcec7fd5a
miumida <46196137+miumida@users.noreply.github.com>
2021-03-09 22:42:28 +0900

    Update notify.py

--- custom_components/line_notify/notify.py
+++ custom_components/line_notify/notify.py
@@ -39,58 +39,44 @@ class LineNotificationService(BaseNotificationService):
 
     def __init__(self, hass, config):
         """Initialize the service."""
-        self._hass = hass
-        self._token   = config.get(CONF_TOKEN)
+        self._hass  = hass
+        self._token  = config.get(CONF_TOKEN)
 
         self._headers = {
             'Authorization' : 'Bearer {}'.format(self._token)
         }
 
 
-    async def send_message(self, message="", **kwargs):
+    def send_message(self, message="", **kwargs):
         """Send a message to specified target."""
-        #websession = async_get_clientsession(self.hass)
 
         if len(message) > 1000:
-            message = 'Message max length is 1000.'
+            _LOGGER.error("[Error] Message max length is 1000. Input message is [ %s ]", message)
 
-        try:
+        data = {
+                "message": message
+               }
 
-            with async_timeout.timeout(10):
+        file = {}
 
-                data = {
-                    "message": message
-                }
+        param = kwargs.get('data', None)
 
-                file = {}
+        if param is not None:
+            #sticker
+            if ( "stickerId" in param ) and ( "stickerPackageId" in param ):
+                data["stickerId"]        = param["stickerId"]
+                data["stickerPackageId"] = param["stickerPackageId"]
 
-                if kwargs is not None:
-                    param = kwargs.get('data', None)
-                    #sticker
-                    if param and param.get('stickerId',False) and param.get('stickerPackageId', False):
-                        stickerId        = kwargs['data']['stickerId']
-                        stickerPackageId = kwargs['data']['stickerPackageId']
+            #imageFile
+            if "imageFile" in param:
+                imageFile = param["imageFile"]
+                file = { 'imageFile': open(imageFile, 'rb') }
 
-                        data["stickerId"]        = stickerId
-                        data["stickerPackageId"] = stickerPackageId
+        url = NOTIFY_LINE_API_URL
 
-                    #imageFile
-                    if param and param.get('imageFile', False):
-                        imageFile = kwargs['data']['imageFile']
+        request = requests.post(url, headers=self._headers, data=data, files=file)
 
-                        file = { 'imageFile': open(imageFile, 'rb') }
-
-                url = NOTIFY_LINE_API_URL
-
-#                request = await websession.post(url, data=data, headers=self._headers)
-
-                request = requests.post(url, headers=self._headers, data=data, files=file)
-
-                if request.status_code != HTTP_OK:
-                    _LOGGER.error( "Error %d on load URL %s", request.status_code, request.url)
-                else:
-                    _LOGGER.debug("Line Notify  send: %s", request.json())
-
-
-        except (asyncio.TimeoutError, aiohttp.ClientError):
-            _LOGGER.error("Timeout for Line Notify")
+        if request.status_code != HTTP_OK:
+            _LOGGER.error( "Error %d on load URL %s", request.status_code, request.url)
+        else:
+            _LOGGER.debug("Line Notify  send: %s", request.json())
