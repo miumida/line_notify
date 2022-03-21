5dd7e3e688e44cc1c803256da3117a3590bee8ae
miumida <46196137+miumida@users.noreply.github.com>
2022-03-21 23:31:05 +0900

    Update const.py

--- custom_components/line_notify/const.py
+++ custom_components/line_notify/const.py
@@ -1,6 +1,6 @@
 DOMAIN = 'line_notify'
-TITLE   = 'LINE Notify'
-VERSION = '1.1.0'
+TITLE  = 'LINE Notify'
+VERSION = '1.1.1'
 
 CONF_TOKEN = 'token'
 NOTIFY_LINE_API_URL = 'https://notify-api.line.me/api/notify'
